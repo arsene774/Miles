654ed2b6798dbbf6edcda6eeeea4dcef8e6086a7
hirwa arsene <hirwasene@gmail.com>
2025-02-01 17:42:58 +0200

    done a hamburger

--- miles.js
+++ miles.js
@@ -76,3 +76,12 @@ dotsNav.addEventListener("click", e => {
     hideShowArrow(slides, prevBtn, nextBtn, targetIndex);
 
 })
+
+
+const hamMenu = document.querySelector('.ham-menu');
+const offScreenMenu = document.querySelector('.off-screen-menu');
+
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active');
+    offScreenMenu.classList.toggle('active');
+})
