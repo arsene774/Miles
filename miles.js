62ac708ea1f6e70ef717b1c2a1f230e79749e6e8
hirwa arsene <hirwasene@gmail.com>
2025-02-08 21:34:18 +0200

    finalizing

--- miles.js
+++ miles.js
@@ -98,24 +98,24 @@ var i;
 // Full-width images
 function one() {
     for (i = 0; i < elements.length; i++) {
-    elements[i].style.msFlex = "100%";  // IE10
-    elements[i].style.flex = "100%";
+    elements[i].style.msFlex = "40%";  // IE10
+    elements[i].style.flex = "40%";
   }
 }
 
 // Two images side by side
 function two() {
   for (i = 0; i < elements.length; i++) {
-    elements[i].style.msFlex = "50%";  // IE10
-    elements[i].style.flex = "50%";
+    elements[i].style.msFlex = "25%";  // IE10
+    elements[i].style.flex = "25%";
   }
 }
 
 // Four images side by side
 function four() {
   for (i = 0; i < elements.length; i++) {
-    elements[i].style.msFlex = "25%";  // IE10
-    elements[i].style.flex = "25%";
+    elements[i].style.msFlex = "5%";  // IE10
+    elements[i].style.flex = "5%";
   }
 }
 
